cea2b372d90181555ebe0667363f6f9ad2850882
sonam-ve <sonam@valuenable.in>
2026-01-07 21:17:28 +0530

    first commit

--- db.py
+++ db.py
@@ -1,6 +1,53 @@
 import sqlite3
 
 def get_db():
-    conn = sqlite3.connect("leave.db", check_same_thread=False)
+    conn = sqlite3.connect("leave_manager.db", check_same_thread=False)
     conn.row_factory = sqlite3.Row
+    cur = conn.cursor()
+
+    # USERS TABLE
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        email TEXT UNIQUE NOT NULL,
+        password TEXT NOT NULL,
+        role TEXT CHECK(role IN ('admin','employee')) NOT NULL
+    )
+    """)
+
+    # LEAVE TYPES
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS leave_types (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT UNIQUE NOT NULL
+    )
+    """)
+
+    # LEAVE BALANCE
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS leave_balance (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        user_id INTEGER,
+        leave_type_id INTEGER,
+        balance INTEGER DEFAULT 0,
+        FOREIGN KEY(user_id) REFERENCES users(id)
+    )
+    """)
+
+    # LEAVE REQUESTS
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS leave_requests (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        user_id INTEGER,
+        leave_type_id INTEGER,
+        from_date TEXT,
+        to_date TEXT,
+        reason TEXT,
+        status TEXT DEFAULT 'pending',
+        created_at TEXT DEFAULT CURRENT_TIMESTAMP
+    )
+    """)
+
+    conn.commit()
     return conn
